6b50fbf61b31900027674860a585622f08801b1d
Hilary Liu <2788370451@qq.com>
2023-07-03 13:55:22 +0800

    day3

---
+++ day3.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: "css",
+});
+useHead({
+  title: "Css Challenge - Easy Scroll"
+})
+</script>
+
+<template>
+  <main class="hl-main">
+    <div class="hl-main-content">
+
+    </div>
+  </main>
+  <CssChallengeFooter :href="'https://github.com/GodlessLiu/css-challenge/blob/main/day3.vue'" :date="'2023/7/4'">
+  </CssChallengeFooter>
+</template>
