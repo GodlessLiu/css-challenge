3ccb26bb768fedb100471c2132807f37b061ca65
Hilary Liu <2788370451@qq.com>
2023-07-04 17:35:05 +0800

    day3

--- day3.vue
+++ day3.vue
@@ -3,16 +3,46 @@ definePageMeta({
   layout: "css",
 });
 useHead({
-  title: "Css Challenge - Easy Scroll"
-})
+  title: "Css Challenge - Easy Scroll",
+});
 </script>
 
 <template>
-  <main class="hl-main">
-    <div class="hl-main-content">
-
-    </div>
+  <main class="hl-main-self">
+    <div class="img img1"></div>
+    <div class="content">CONTENT1</div>
+    <div class="img img2"></div>
+    <div class="content">CONTENT2</div>
+    <div class="img img3"></div>
+    <div class="content">CONTENT3</div>
   </main>
-  <CssChallengeFooter :href="'https://github.com/GodlessLiu/css-challenge/blob/main/day3.vue'" :date="'2023/7/4'">
+  <CssChallengeFooter
+    :href="'https://github.com/GodlessLiu/css-challenge/blob/main/day3.vue'"
+    :date="'2023/7/4'"
+  >
   </CssChallengeFooter>
 </template>
+<style lang="css" scoped>
+.hl-main-self > div {
+  height: 100vh;
+}
+.img {
+  background-attachment: fixed;
+  background-position: center center;
+  background-size: cover;
+}
+.img1 {
+  background-image: url(/css-challenge/1.jpg);
+}
+.img2 {
+  background-image: url(/css-challenge/2.jpg);
+}
+.img3 {
+  background-image: url(/css-challenge/3.jpg);
+}
+.content {
+  line-height: 100vh;
+  text-align: center;
+  font-size: 48px;
+}
+</style>
