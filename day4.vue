cc3804c8732c425bd78f6306b6cdc074816ae432
Hilary Liu <2788370451@qq.com>
2023-07-04 19:29:09 +0800

    day4

---
+++ day4.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: "css",
+});
+useHead({
+  title: "Css Challenge - Drop-down Menu",
+});
+</script>
+<template>
+  <main class="hl-main">
+    <div class="hl-main-cotent">day4</div>
+  </main>
+  <CssChallengeFooter
+    :href="'https://github.com/GodlessLiu/css-challenge/blob/main/day4.vue'"
+    :date="'2023/7/5'"
+  >
+  </CssChallengeFooter>
+</template>
+
+<style lang="css" scoped></style>
