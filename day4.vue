696f9086f8570a55fee379b6f8e9bbde1a1bf0d4
Hilary Liu <2788370451@qq.com>
2023-07-05 18:02:18 +0800

    day5

--- day4.vue
+++ day4.vue
@@ -8,7 +8,18 @@ useHead({
 </script>
 <template>
   <main class="hl-main">
-    <div class="hl-main-cotent">day4</div>
+    <div class="hl-main-cotent">
+      <div class="menu">
+        <a href="" class="menu-a">菜单</a>
+        <div class="menu-list">
+          <ul>
+            <li><a href="" class="menu-list-a">我的</a></li>
+            <li><a href="" class="menu-list-a">交流</a></li>
+            <li><a href="" class="menu-list-a">主页</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </main>
   <CssChallengeFooter
     :href="'https://github.com/GodlessLiu/css-challenge/blob/main/day4.vue'"
@@ -17,4 +28,33 @@ useHead({
   </CssChallengeFooter>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.menu {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.menu-a {
+  height: 25px;
+}
+.menu-a:hover + .menu-list,
+.menu-list:hover {
+  visibility: visible;
+}
+.menu-list {
+  visibility: hidden;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  position: absolute;
+  margin-top: 30px;
+  border-radius: 4px;
+  padding: 12px;
+  transition: visibility 0.2s;
+}
+.menu-list-a {
+  border-radius: 4px;
+  padding: 4px 8px;
+}
+.menu-list-a:hover {
+  background-color: rgba(0, 0, 0, 0.16);
+}
+</style>
---
+++ day5.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: "css",
+});
+useHead({
+  title: "Css Challenge - Swith Tabs",
+});
+</script>
+
+<template>
+  <main class="hl-main">
+    <div class="hl-main-content">day5</div>
+  </main>
+  <CssChallengeFooter
+    :href="'https://github.com/GodlessLiu/css-challenge/blob/main/day5.vue'"
+    :date="'2023/7/6'"
+  >
+  </CssChallengeFooter>
+</template>
+
+<style lang="css" scoped></style>
